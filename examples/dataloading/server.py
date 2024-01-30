196c841b4f0d1a0d91ceac7ffd67289ad46152ca
Cong Wang <cwang31@stanford.edu>
2024-01-29 17:34:20 -0800

    Providing examples on streaming hdf5.

--- examples/dataloading/server.py
+++ examples/dataloading/server.py
@@ -7,6 +7,9 @@
 from flask import Flask, request, Response
 import os
 import msgpack
+import io
+import h5py
+import numpy as np
 from maxie.datasets.psana_utils import PsanaImg
 
 app = Flask(__name__)
@@ -41,6 +44,29 @@ def fetch_data():
         'pid': pid
     }
     response_data = msgpack.packb(response_dict, use_bin_type=True)
+    return Response(response_data, mimetype='application/octet-stream')
+
+
+@app.route('/fetch-hdf5', methods=['POST'])
+def fetch_hdf5():
+    exp = request.json.get('exp')
+    run = request.json.get('run')
+    access_mode = request.json.get('access_mode')
+    detector_name = request.json.get('detector_name')
+    event = request.json.get('event')
+    mode = request.json.get('mode', 'calib')
+
+    psana_img = get_psana_img(exp, run, access_mode, detector_name)
+    data = psana_img.get(event, None, mode)
+
+    # Serialize data using BytesIO and hdf5...
+    with io.BytesIO() as hdf5_bytes:
+        with h5py.File(hdf5_bytes, 'w') as hdf5_file_handle:
+            hdf5_file_handle.create_dataset('data', data = data)
+            hdf5_file_handle.create_dataset('pid' , data = np.array([pid]))
+
+        response_data = hdf5_bytes.getvalue()
+
     return Response(response_data, mimetype='application/octet-stream')
 
 if __name__ == "__main__":
