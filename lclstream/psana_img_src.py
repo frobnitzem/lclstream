4262e425612cde24cba0374868ecc746c90d1c2f
Valerio Mariani <valmar@slac.stanford.edu>
2024-07-16 16:51:18 -0700

    Modified psana wrapper to support smd access mode

--- lclstream/psana_img_src.py
+++ lclstream/psana_img_src.py
@@ -1,21 +1,26 @@
 import numpy as np
 
-from .psana_stub import DataSource, Detector
+from psana import DataSource, MPIDataSource, Detector
 
 class PsanaImgSrc:
     """
     It serves as an image accessing layer based on the data
-    management system psana in LCLS.  
+    management system psana in LCLS (idx access mode)
     """
 
-    def __init__(self, exp, run, mode, detector_name):
+    def __init__(self, exp, run, access_mode, detector_name):
         # Boilerplate code to access an image
         # Set up data source
-        self.datasource_id = f"exp={exp}:run={run}:{mode}"
-        self.datasource    = DataSource( self.datasource_id )
-        self.run_current   = next(self.datasource.runs())
-        self.timestamps    = self.run_current.times()
-
+        self.access_mode = access_mode
+        self.datasource_id = f"exp={exp}:run={run}:{access_mode}"
+        if self.access_mode == "idx":
+            self.datasource    = DataSource(self.datasource_id )
+            self.run_current   = next(self.datasource.runs())
+            self.events    = self.run_current.times()
+        else:
+            self.datasource    = MPIDataSource(self.datasource_id )
+            self.events        = self.datasource.events()
+            
         # Set up detector
         self.detector = Detector(detector_name)
 
@@ -26,16 +31,22 @@ class PsanaImgSrc:
                       "mask"  : self.detector.mask, }
 
     def __len__(self):
-        return len(self.timestamps)
+        if access_mode == "idx":
+            return len(self.timestamps)
+        else:
+            raise NotImplementedError
 
     def __call__(self, mode, id_panel = None):
         # Only two modes are supported...
         assert mode in ("raw", "calib", "image"), \
                 f"Mode {mode} is not allowed!!!  Only 'raw', 'calib' and 'image' are supported."
-        
-        for timestamp in self.timestamps:
-            event = self.run_current.event(timestamp)
-            data = self.read[mode](event)
+
+        for event in self.events:
+            if self.access_mode == "idx":
+                event_data = self.run_current.event(event)
+            else:
+                event_data = event
+            data = self.read[mode](event_data)
             # assembling a multi-panel image:
             #img = self.read[mode](event, multipanel)
             yield data[int(id_panel)] if id_panel is not None else data
@@ -48,5 +59,4 @@ class PsanaImgSrc:
                                                    unbond      = True,
                                                    unbondnbrs  = True,
                                                    unbondnbrs8 = False).astype(np.uint16)
-
 
