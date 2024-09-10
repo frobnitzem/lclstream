f4500e8465cc9a7819056437624fb5b5c04cb1c8
David M. Rogers <predictivestatmech@gmail.com>
2024-09-10 15:49:49 -0400

    Bumped version.

--- lclstream/nng.py
+++ lclstream/nng.py
@@ -4,7 +4,7 @@ import logging
 _logger = logging.getLogger(__name__)
 
 import stream
-from pynng import Push0, Pull0
+from pynng import Push0, Pull0, Timeout, ConnectionRefused # type: ignore[import-untyped]
 
 clock0 = lambda: {'count': 0, 'size': 0, 'wait': 0, 'time': time.time()}
 def rate_clock(state, sz):
@@ -19,6 +19,7 @@ def rate_clock(state, sz):
 send_opts : dict[str,int] = {
      #"send_buffer_size": 32 # send blocks if 32 messages queue up
 }
+recv_options = {"recv_timeout": 5000}
 
 @stream.stream
 def pusher(gen : Iterator[bytes], addr : str, ndial : int
@@ -45,6 +46,47 @@ def pusher(gen : Iterator[bytes], addr : str, ndial : int
         _logger.error("Unable to connect to %s - %s", addr, e)
 
 @stream.source
+def puller(addr : str, ndial : int) -> Iterator[bytes]:
+    assert ndial >= 0
+
+    done = 0
+    started = 0
+    def show_open(pipe):
+        nonlocal started
+        _logger.info("Pull: pipe opened")
+        started += 1
+    def show_close(pipe):
+        nonlocal done
+        _logger.info("Pull: pipe closed")
+        done += 1
+
+    options = dict(recv_options)
+    if ndial == 0:
+        options["listen"] = addr
+    try:
+        with Pull0(**options) as pull:
+            pull.add_post_pipe_connect_cb(show_open)
+            pull.add_post_pipe_remove_cb(show_close)
+            for dial in range(ndial):
+                pull.dial(addr, block=True)
+            if ndial == 0:
+                _logger.info("Pull: waiting for connection")
+            else:
+                _logger.info("Connected to %s x %d - starting recv.",
+                              addr, ndial)
+
+            while started == 0 or (done != started):
+                try:
+                    msg = pull.recv()
+                    yield msg
+                except Timeout:
+                    if started:
+                        _logger.debug("Pull: slow input")
+
+    except ConnectionRefused as e:
+        _logger.error("Unable to connect to %s - %s", addr, e)
+
+@stream.source
 def file_chunks(fname, chunksz=1024*1024) -> Iterator[bytes]:
     with open(fname, 'rb') as f:
         while True:
@@ -53,5 +95,17 @@ def file_chunks(fname, chunksz=1024*1024) -> Iterator[bytes]:
                 break
             yield data
 
-
+@stream.stream
+def file_writer(gen : Iterator[bytes],
+                fname : str,
+                append : bool = False
+               ) -> Iterator[int]:
+    if append:
+        mode = 'ab'
+    else:
+        mode = 'wb'
+    with open(fname, mode) as f:
+        for data in gen:
+            sz = f.write(data)
+            yield sz
 
